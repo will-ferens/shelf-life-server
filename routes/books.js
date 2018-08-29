cf7dc24775b402694093bc69d7666f983b9bc5dd
_dream <will.ferens@gmail.com>
2018-08-29 16:50:08 -0600

    simplified add route

--- routes/books.js
+++ routes/books.js
@@ -13,14 +13,7 @@ router.get('/', checkAuth, (req, res, next) => {
     Book.find({user: userId})
         .exec()
         .then(result => {
-            if(result.length >= 1){
-                res.status(200).json(result)
-            } else {
-                res.status(200).json({
-                    message: 'Add some books to get started!',
-                })
-            }
-            
+                json(result)
         })
         .catch(err => {
             res.status(500).json({message: err})
